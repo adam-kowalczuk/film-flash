67cdf7289177a3b09e251ed69e9567dbfc7e6df1
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-06-03 12:30:51 -0700

    Add code for dynamically rendering movie title and description

--- api.js
+++ api.js
@@ -13,10 +13,17 @@ fetch(
   .then((response) => response.json())
   .then((response) => {
     console.log(response.results);
-    const randomMovie = Math.floor(Math.random() * 20);
-    const backdropPath = response.results[`${randomMovie}`].backdrop_path;
+    const randomNumber = Math.floor(Math.random() * 20);
+    const randomMovie = response.results[randomNumber];
+    const backdropPath = randomMovie.backdrop_path;
+
     const mainElement = document.querySelector("main");
+    const title = document.getElementById("title");
+    const description = document.getElementById("description");
+
     mainElement.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${backdropPath})`;
+    title.innerHTML = randomMovie.title;
+    description.innerHTML = randomMovie.overview;
     mainElement.classList.add("main-fade-in");
   })
   .catch((err) => console.error(err));
