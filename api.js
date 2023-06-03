f4b9b809ff4901563fbbbd7e0135be8e2a6f6816
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-06-02 20:57:09 -0700

    Remove superfluous code

--- api.js
+++ api.js
@@ -6,17 +6,14 @@ const options = {
   }
 };
 
-// const randomPage = Math.floor(Math.random() * 92) + 1;
-
 fetch(
-  // "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
-  `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1`,
+  "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
   options
 )
   .then((response) => response.json())
   .then((response) => {
-    console.log(response.results.length);
-    const randomMovie = Math.floor(Math.random() * 20) + 1;
+    console.log(response.results);
+    const randomMovie = Math.floor(Math.random() * 20);
     const backdropPath = response.results[`${randomMovie}`].backdrop_path;
     const mainElement = document.querySelector("main");
     mainElement.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${backdropPath})`;
