168f17cee464fc221f0b6cd055641cb50eb83047
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-06-08 13:49:52 -0700

    Add fetch request for grabbing movie trailer

--- api.js
+++ api.js
@@ -21,12 +21,29 @@ fetch(
     const title = document.getElementById("title");
     const description = document.getElementById("description");
     const movieLink = document.getElementById("movie-link");
+    const movieId = randomMovie.id;
 
-    mainElement.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${backdropPath})`;
-    title.innerHTML = randomMovie.title;
-    movieLink.href = `https://www.themoviedb.org/movie/${randomMovie.id}`;
-    description.innerHTML = randomMovie.overview;
+    // Update the fetch URL to retrieve the specific movie details, including videos
+    fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?language=en-US&append_to_response=videos`,
+      options
+    )
+      .then((response) => response.json())
+      .then((movieResponse) => {
+        // Retrieve the movie trailer details from the fetched data
+        const trailer = movieResponse.videos.results.find((video) =>
+          /trailer/i.test(video.name)
+        );
+        const trailerKey = trailer.key;
+        const trailerUrl = `https://www.youtube.com/watch?v=${trailerKey}`;
 
-    mainElement.classList.add("main-fade-in");
+        mainElement.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${backdropPath})`;
+        title.innerHTML = randomMovie.title;
+        movieLink.href = trailerUrl;
+        description.innerHTML = randomMovie.overview;
+
+        mainElement.classList.add("main-fade-in");
+      })
+      .catch((err) => console.error(err));
   })
   .catch((err) => console.error(err));
