95379a5dc27a111b2d55a07fbd44884064a64bcc
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-06-12 16:00:01 -0700

    Create kebabCase function, add info and showtimes links

--- api.js
+++ api.js
@@ -6,6 +6,12 @@ const options = {
   }
 };
 
+const kebabCase = function(title) {
+  const punctuationPattern = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+  const newTitle = title.replaceAll(punctuationPattern, "");
+  return newTitle.toLowerCase().split(" ").join("-");
+};
+
 fetch(
   "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
   options
@@ -17,10 +23,14 @@ fetch(
     const randomMovie = response.results[randomNumber];
     const backdropPath = randomMovie.backdrop_path;
 
+    const kebabTitle = kebabCase(randomMovie.title);
+
     const mainElement = document.querySelector("main");
     const title = document.getElementById("title");
     const description = document.getElementById("description");
+    const info = document.getElementById("info");
     const trailerLink = document.getElementById("trailer-link");
+    const showtimes = document.getElementById("showtimes");
     const movieId = randomMovie.id;
 
     // Update the fetch URL to retrieve the specific movie details, including videos
@@ -39,8 +49,10 @@ fetch(
 
         mainElement.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${backdropPath})`;
         title.innerHTML = randomMovie.title;
-        trailerLink.href = trailerUrl;
         description.innerHTML = randomMovie.overview;
+        info.href = `https://www.themoviedb.org/movie/${movieId}`;
+        trailerLink.href = trailerUrl;
+        showtimes.href = `https://www.cinemaclock.com/movies/${kebabTitle}-2023/showtimes`;
 
         mainElement.classList.add("main-fade-in");
       })
