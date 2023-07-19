02391b73d3f192664b03d366966d26c853dabab8
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-07-19 13:36:56 -0700

    Remove index and test scripts as they are no longer needed

--- test.js
+++
@@ -1,6 +0,0 @@
-const id = 1070802;
-
-fetch(`https://film-flash.netlify.app/.netlify/functions/videos?id=${id}`)
-  .then((res) => res.json())
-  .then((data) => console.log("Test Data: ", data.videos.results))
-  .catch((error) => console.error("Error", error));
