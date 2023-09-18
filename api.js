e3c7f5d135a2491536eb6bebb08574c9e1fcb7bf
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-09-18 08:00:15 -0700

    Add variable for current year

--- api.js
+++ api.js
@@ -31,13 +31,14 @@ fetch("https://film-flash.netlify.app/.netlify/functions/now-playing")
         const info = document.getElementById("info");
         const trailerLink = document.getElementById("trailer-link");
         const showtimes = document.getElementById("showtimes");
+        const currentYear = new Date().getFullYear();
 
         mainElement.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${backdropPath})`;
         title.innerHTML = randomMovie.title;
         description.innerHTML = randomMovie.overview;
         info.href = `https://www.themoviedb.org/movie/${movieId}`;
         trailerLink.href = trailerUrl;
-        showtimes.href = `https://www.cinemaclock.com/movies/${kebabTitle}-2023/showtimes`;
+        showtimes.href = `https://www.cinemaclock.com/movies/${kebabTitle}-${currentYear}/showtimes`;
 
         mainElement.classList.add("main-fade-in");
       })
